4a1fdbdefa12e62233dedd854fe53503005961da
MarcosVFG <marcosvfrontend@gmail.com>
2023-03-24 20:59:13 -0300

    Second Commit

--- cart/src/app/page.jsx
+++ cart/src/app/page.jsx
@@ -1,50 +1,82 @@
+'use client';
+
+import { useState } from 'react'
 import styles from './page.module.css'
 import fundo from '/public/bg-main-desktop.png'
 import cartaoFrente from '/public/bg-card-front.png'
 import cartaoTras from '/public/bg-card-back.png'
 import Image from 'next/image'
 
-
 export default function Home() {
+
+    const [valor, setValor] = useState('');
+    const handleChange = (event) => {
+      setValor(event.target.value);
+    };
+
+    const [valor2, setValor2] = useState('');
+    const handleChange2 = (event) => {
+      setValor2(event.target.value);
+    };
+
+    const [valor3, setValor3] = useState('');
+    const handleChange3 = (event) => {
+      setValor3(event.target.value);
+    };
+
+    const [valor4, setValor4] = useState('');
+    const handleChange4 = (event) => {
+      setValor4(event.target.value);
+    };
+
+    const [showText, setShowText] = useState(false);
+    const handleButtonClick = () => {
+      setShowText(true)};
+
   return (
     <main className={styles.principal}>
       <div>
         <Image className={styles.fundoAzul} src={fundo}/>
         <div>
+          <h1 className={styles.textin}>{showText && valor || '0000 0000 0000 0000'}</h1>
+          <h2 className={styles.textin2}>{showText && valor2 || 'John Dell'}</h2>
+          <h2 className={styles.textin3}>{showText && valor3 || '00'}</h2>
+          <h2 className={styles.textin4}>/</h2>
+          <h2 className={styles.textin5}>{showText && valor4 || '00'}</h2>
           <Image className={styles.cartaoFrente} src={cartaoFrente}/>
           <Image className={styles.cartaoTras} src={cartaoTras}/>
         </div>
       </div>
-
       <div className={styles.formularioDiv}>
-        <form action="" className={styles.formulario}>
+        <div className={styles.formulario}>
 
           <div className={styles.divForm}>
-            <label for="nome">Nome no Cartão</label>
-            <input type="text" id="nomeid" placeholder="John Dell" required="required" name="nome" />
+            <p>Nome no Cartão</p>
+            <input type="text" id="valor" placeholder="John Dell" required="required" name="valor" value={valor2} onChange={handleChange2} />
            </div>
 
            <div className={styles.divForm}>
-            <label for="numero">Numero do Cartão</label>
-            <input type="number" id="numeroid" placeholder="1234 4567 8910 1112" required="required" name="numero" />
+            <p>Numero do Cartão</p>
+            <input type="number" id="numeroid" placeholder="1234 4567 8910 1112" required="required" name="numero" value={valor} onChange={handleChange} />
            </div>
           
            <div className={styles.dataPlusCvc}>
             <div className={styles.divForm}>
-                <label for="data">Data de Validade</label>
-                <input type="number" id="dataid" placeholder="12" required="required" name="data" />
-                <input type="number" id="datatwoid" placeholder="25" required="required" name="dataTwo" />
+                <p>Data de Validade</p>
+                <input type="number" id="dataid" placeholder="12" required="required" name="data" value={valor3} onChange={handleChange3} />
             </div>
 
+            <input type="number" id="datatwoid" placeholder="25" required="required" name="dataTwo" value={valor4} onChange={handleChange4}/>
+
             <div className={styles.divForm}>
-              <label for="seguranca">CVC</label>
+              <p>CVC</p>
               <input type="number" id="segurancaid" placeholder="123" required="required" name="seguranca" />
             </div>
            </div>
-           <button type="submit">Confirmar</button>
+           <button onClick={handleButtonClick}>Confirmar</button>
 
-        </form>
+        </div>
       </div>
     </main>
   )
-}
+};
